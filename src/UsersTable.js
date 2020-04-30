df750b67571d49691f4e3d0a7e19890ef94c6cd2
Esteban Puentes <virtualevan@gmail.com>
2020-04-30 13:36:38 +0200

    Add multi licenses conditional rendering

--- src/UsersTable.js
+++ src/UsersTable.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Table } from 'semantic-ui-react'
 
-function UserRow({
+function SingleLicenseRow({
     username,
     hostname,
     display,
@@ -9,9 +9,50 @@ function UserRow({
     server,
     port,
     handle,
-    checkout,
-    num_licenses
+    checkout
   }) {
+
+  return (
+    <Table.Row>
+      <Table.Cell>
+        {username}
+      </Table.Cell>
+      <Table.Cell>
+        {hostname}
+      </Table.Cell>
+      <Table.Cell>
+        {display}
+      </Table.Cell>
+      <Table.Cell>
+        {version}
+      </Table.Cell>
+      <Table.Cell>
+        {server}
+      </Table.Cell>
+      <Table.Cell>
+        {port}
+      </Table.Cell>
+      <Table.Cell>
+        {handle}
+      </Table.Cell>
+      <Table.Cell>
+        {checkout}
+      </Table.Cell>
+    </Table.Row>
+  );
+}
+
+function MultiLicenseRow({
+  username,
+  hostname,
+  display,
+  version,
+  server,
+  port,
+  handle,
+  checkout,
+  num_licenses
+}) {
 
   return (
     <Table.Row>
@@ -46,11 +87,14 @@ function UserRow({
   );
 }
 
-export default function UsersTable({usersData}) {
+export default function UsersTable({userList = []}) {
 
-  if (usersData.length === 0) {
+  if (userList.length === 0) {
     return null;
   }
+
+  // Check if multiple licenses are granted for this feature
+  const isMultiLicense = 'num_licenses' in userList[0]
 
   return (
     <Table striped>
@@ -64,25 +108,40 @@ export default function UsersTable({usersData}) {
           <Table.HeaderCell>Port</Table.HeaderCell>
           <Table.HeaderCell>Handle</Table.HeaderCell>
           <Table.HeaderCell>Checkout</Table.HeaderCell>
-          <Table.HeaderCell>Num. licenses</Table.HeaderCell>
+          {isMultiLicense && (<Table.HeaderCell>Num. licenses</Table.HeaderCell>)}
         </Table.Row>
       </Table.Header>
 
       <Table.Body>
-        {usersData.map(user => (
-          <UserRow
-            key={user.username}
-            username={user.username}
-            hostname={user.hostname}
-            display={user.display}
-            version={user.version}
-            server={user.server}
-            port={user.port}
-            handle={user.handle}
-            checkout={user.checkout}
-            num_licenses={user.num_licenses}
-          />
-        ))}
+        {isMultiLicense
+          ? userList.map((user, key) => (
+              <MultiLicenseRow
+                key={key}
+                username={user.username}
+                hostname={user.hostname}
+                display={user.display}
+                version={user.version}
+                server={user.server}
+                port={user.port}
+                handle={user.handle}
+                checkout={user.checkout}
+                num_licenses={user.num_licenses}
+              />
+            ))
+          : userList.map((user, key) => (
+              <SingleLicenseRow
+                key={key}
+                username={user.username}
+                hostname={user.hostname}
+                display={user.display}
+                version={user.version}
+                server={user.server}
+                port={user.port}
+                handle={user.handle}
+                checkout={user.checkout}
+              />
+            ))
+        }
       </Table.Body>
     </Table>
   );
