5972e936a6e5b0c4d131ddc82baaf9c27446e09d
Esteban Puentes <virtualevan@gmail.com>
2020-11-10 22:50:31 +0100

    Update nave menu

--- src/NavigationMenu.js
+++ src/NavigationMenu.js
@@ -8,19 +8,15 @@ export default function NavigationMenu() {
       <Menu.Item active as="a">
         Dashboard
       </Menu.Item>
-      <Menu.Item as="a">Option 1</Menu.Item>
-      <Menu.Item as="a">Option 2</Menu.Item>
-      <Menu.Item as="a">Option 3</Menu.Item>
-      <Divider hidden />
-      <Menu.Item as="a">Option 4</Menu.Item>
-      <Menu.Item as="a">Option 5</Menu.Item>
-      <Menu.Item as="a">Option 6</Menu.Item>
-      <Menu.Item as="a">Option 7</Menu.Item>
-      <Menu.Item as="a">Option 8</Menu.Item>
-      <Divider hidden />
-      <Menu.Item as="a">Option 9</Menu.Item>
-      <Menu.Item as="a">Option 10</Menu.Item>
-      <Menu.Item as="a">Option 11</Menu.Item>
+      <Menu.Item as="a">
+        Raw data
+      </Menu.Item>
+      <Menu.Item as="a">
+        Service overview
+      </Menu.Item>
+      <Menu.Item as="a">
+        User permissions
+      </Menu.Item>
     </Menu>
   );
 
