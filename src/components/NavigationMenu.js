518795b5b0794ef715cdb61bf9189cde2ec7c242
Esteban Puentes <virtualevan@gmail.com>
2021-01-18 23:29:16 +0100

    Add history to the nav menu

--- src/components/NavigationMenu.js
+++ src/components/NavigationMenu.js
@@ -1,19 +1,43 @@
 import React from 'react';
 import {Menu} from 'semantic-ui-react';
+import {useHistory} from 'react-router';
 
 export default function NavigationMenu() {
+  const history = useHistory();
+
   return (
     <Menu vertical borderless fluid text>
-      <Menu.Item as="a" href="/" active>
+      <Menu.Item
+        as="a"
+        onClick={() => {
+          history.push('/');
+        }}
+        active
+      >
         Dashboard
       </Menu.Item>
-      <Menu.Item as="a" href="/">
+      <Menu.Item
+        as="a"
+        onClick={() => {
+          history.push('/');
+        }}
+      >
         Raw data
       </Menu.Item>
-      <Menu.Item as="a" href="/service">
+      <Menu.Item
+        as="a"
+        onClick={() => {
+          history.push('/service');
+        }}
+      >
         Service overview
       </Menu.Item>
-      <Menu.Item as="a" href="/users">
+      <Menu.Item
+        as="a"
+        onClick={() => {
+          history.push('/users');
+        }}
+      >
         User management
       </Menu.Item>
     </Menu>
