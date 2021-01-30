5015847baa07838ed46cd397d2b7741795a60978
Esteban Puentes <virtualevan@gmail.com>
2021-01-30 16:14:18 +0100

    Change layout to a centered one

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
-import {Grid} from 'semantic-ui-react';
+import {Container} from 'semantic-ui-react';
+import './App.module.scss';
 
-import './App.css';
-import NavigationMenu from './NavigationMenu';
 import StatusPage from './status/StatusPage';
 import ServicePage from './ServicePage';
 import UsersPage from './users/UsersPage';
@@ -11,19 +10,13 @@ import UsersPage from './users/UsersPage';
 function App() {
   return (
     <Router>
-      <Grid padded>
-        <Grid.Column tablet={3} computer={3} only="tablet computer" id="sidebar">
-          <NavigationMenu />
-        </Grid.Column>
-
-        <Grid.Column mobile={16} tablet={13} computer={13} floated="right" id="content">
-          <Switch>
-            <Route exact path="/" component={StatusPage} />
-            <Route exact path="/service" component={ServicePage} />
-            <Route exact path="/users" component={UsersPage} />
-          </Switch>
-        </Grid.Column>
-      </Grid>
+      <Container>
+        <Switch>
+          <Route exact path="/" component={StatusPage} />
+          <Route exact path="/service" component={ServicePage} />
+          <Route exact path="/users" component={UsersPage} />
+        </Switch>
+      </Container>
     </Router>
   );
 }
